644ea7632315534a87c84a11051121416a23fb49
reinaldoamoras <reinaldo.amoras@icloud.com>
2026-01-29 18:43:17 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      API_KEY: JSON.stringify(process.env.API_KEY || '')
-    }
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       input: {
         main: './index.html'
