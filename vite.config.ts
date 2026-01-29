ac8483682b43371189a641478b0d7c8df2f6a120
reinaldoamoras <reinaldo.amoras@icloud.com>
2026-01-29 12:32:38 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,15 @@ export default defineConfig({
       API_KEY: JSON.stringify(process.env.API_KEY || '')
     }
   },
+  server: {
+    port: 3000
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 });
